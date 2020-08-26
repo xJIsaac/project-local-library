4e6ebb5b84e1754002460b8e874c4d8896cbf045
Wes Reid <bwreid@gmail.com>
2020-08-25 17:41:21 -0700

    Add src/ files

---
+++ public/src/accounts.js
@@ -0,0 +1,14 @@
+function findAccountById(accounts, id) {}
+
+function sortAccountsByLastName(accounts) {}
+
+function numberOfBorrows(account, books) {}
+
+function booksInPossession(account, books, authors) {}
+
+module.exports = {
+  findAccountById,
+  sortAccountsByLastName,
+  numberOfBorrows,
+  booksInPossession,
+};
---
+++ public/src/home.js
@@ -0,0 +1,20 @@
+function totalBooksCount(books) {}
+
+function totalAccountsCount(accounts) {}
+
+function booksBorrowedCount(books) {}
+
+function mostCommonGenres(books) {}
+
+function mostPopularBooks(books) {}
+
+function mostPopularAuthors(books, authors) {}
+
+module.exports = {
+  totalBooksCount,
+  totalAccountsCount,
+  booksBorrowedCount,
+  mostCommonGenres,
+  mostPopularBooks,
+  mostPopularAuthors,
+};
