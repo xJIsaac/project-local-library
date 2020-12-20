df1ccb6c8bd9e3b74c8aafb070a3514b704fa393
Jaime Moreno <j7isaac@gmail.com>
2020-12-20 14:35:41 -0600

    Completed function "sortAccountsByLastName"

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -2,7 +2,11 @@ function findAccountById(accounts, id) {
   return accounts.find( acct => acct.id == id)
 }
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((acctA, acctB) => 
+    acctA.name.last.toLowerCase() > acctB.name.last.toLowerCase() ? 1 : -1
+  )
+}
 
 function numberOfBorrows(account, books) {}
 
