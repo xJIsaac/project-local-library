9e809f091739fc8c3549eb7495df2b1f7c1447b8
Jaime Moreno <j7isaac@gmail.com>
2020-12-20 19:14:19 -0600

    Completed "findBookById"

--- public/src/books.js
+++ public/src/books.js
@@ -2,7 +2,9 @@ function findAuthorById(authors, id) {
   return authors.find(author => author.id == id)
 }
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find(book => book.id == id)
+}
 
 function partitionBooksByBorrowedStatus(books) {}
 
