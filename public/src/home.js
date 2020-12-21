6d6fe0276ca573ca67f766e116b9c4ee88ba090a
Jaime Moreno <j7isaac@gmail.com>
2020-12-21 03:01:52 -0600

    Completed "getMostCommonGenres" function

--- public/src/home.js
+++ public/src/home.js
@@ -10,7 +10,15 @@ function booksBorrowedCount(books) {
   return books.filter(book => book.borrows[0].returned == false).length
 }
 
-function getMostCommonGenres(books) {}
+function getMostCommonGenres(books) {
+  const result = books.reduce((acc, book) => {
+    const thisGenre = {name: book.genre, count: 1}
+    const existingGenre = acc.find( genre => genre.name == thisGenre.name)
+    existingGenre ? existingGenre.count++ : acc.push(thisGenre)
+    return acc;
+  }, [])
+  return result.sort((genreA, genreB) => genreA.count < genreB.count ? 1 : -1).slice(0, 5)
+}
 
 function getMostPopularBooks(books) {}
 
