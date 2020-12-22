1e77b11a01e6a7f5143f25ce2aa593e26d22984c
Jaime Moreno <j7isaac@gmail.com>
2020-12-21 19:37:23 -0600

    Refactored 2 Functions
    Refactored:
    getMostCommonGenres
    getMostPopularBooks

--- public/src/home.js
+++ public/src/home.js
@@ -13,15 +13,23 @@ function totalAccountsCount(accounts) {
 function booksBorrowedCount(books) {
   return books.filter(book => book.borrows[0].returned == false).length
 }
+
 function sortAndGiveTopFive(array) {
   return array.sort((itemA, itemB) => itemA.count < itemB.count ? 1 : -1).slice(0, 5)
 }
 
 function getMostCommonGenres(books) {
+  const result = books.reduce((acc, book) => {
+    const existingGenre = acc.find( genre => genre.name == book.genre)
+    existingGenre ? existingGenre.count++ : acc.push({name: book.genre, count: 1})
+    return acc;
+  }, [])
+  return sortAndGiveTopFive(result)
+}
+
 function getMostPopularBooks(books) {
   const result = books.reduce((acc, book) => {
-    const newObj = {name: book["title"], count: book.borrows.length}
-    acc.push(newObj)
+    acc.push({name: book["title"], count: book.borrows.length})
     return acc;
   }, [])
   return sortAndGiveTopFive(result)
