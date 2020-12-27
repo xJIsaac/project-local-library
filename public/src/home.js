f12f04dd3ecb501c9ac8a8d5132070e444db1939
Jaime Moreno <j7isaac@gmail.com>
2020-12-27 15:40:21 -0600

    Linted home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,50 +1,51 @@
 const {
   findAuthorById,
-} = require("./books.js");
+} = require('./books.js');
 
 function totalBooksCount(books) {
-  return books.length
+  return books.length;
 }
 
 function totalAccountsCount(accounts) {
-  return accounts.length
+  return accounts.length;
 }
 
 function booksBorrowedCount(books) {
-  return books.filter(book => book.borrows[0].returned == false).length
+  return books.filter((book) => book.borrows[0].returned === false).length;
 }
 
 function sortAndGiveTopFive(array) {
-  return array.sort((itemA, itemB) => itemA.count < itemB.count ? 1 : -1).slice(0, 5)
+  return array.sort((itemA, itemB) => (itemA.count < itemB.count ? 1 : -1)).slice(0, 5);
 }
 
 function getMostCommonGenres(books) {
   const result = books.reduce((acc, book) => {
-    const existingGenre = acc.find( genre => genre.name == book.genre)
-    existingGenre ? existingGenre.count++ : acc.push({name: book.genre, count: 1})
+    const existingGenre = acc.find((genre) => genre.name === book.genre);
+    // eslint-disable-next-line no-unused-expressions
+    existingGenre ? existingGenre.count += 1 : acc.push({ name: book.genre, count: 1 });
     return acc;
-  }, [])
-  return sortAndGiveTopFive(result)
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 function getMostPopularBooks(books) {
   const result = books.reduce((acc, book) => {
-    acc.push({name: book["title"], count: book.borrows.length})
+    acc.push({ name: book.title, count: book.borrows.length });
     return acc;
-  }, [])
-  return sortAndGiveTopFive(result)
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 function getMostPopularAuthors(books, authors) {
   const result = books.reduce((acc, book) => {
-    const {name: {first, last}} = findAuthorById(authors, book.authorId)
+    const { name: { first, last } } = findAuthorById(authors, book.authorId);
     acc.push({
       name: `${first} ${last}`,
-      count: book.borrows.length
-    })
+      count: book.borrows.length,
+    });
     return acc;
-  }, [])
-  return sortAndGiveTopFive(result)
+  }, []);
+  return sortAndGiveTopFive(result);
 }
 
 module.exports = {
